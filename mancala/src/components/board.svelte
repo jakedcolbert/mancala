fa52a84ed1a75abc616052e3784fe73435d5802d
jakedcolbert <jake.colbert@gordon.edu>
2023-07-02 19:07:20 -0400

    reactive colors

--- mancala/src/components/board.svelte
+++ mancala/src/components/board.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
 	// number of pits per side
 	export const pits: number = 5;
+	export const cols = 2;
+	const boardColor = '#FFFFFF';
+	const pitColor = '#FF5733';
 </script>
 
-<div class="box-border rounded-3xl bg-white w-32">
+<div class="box-border rounded-3xl" style:background-color={boardColor}>
 	<div class="p-2">
-		<div class="m-autoe h-12 bg-red-400 rounded-full" />
-		<div class="grid grid-cols-2">
-			<div class="flex flex-col items-center">
-				{#each Array(pits) as _, i}
-					<div class="m-1.5 w-12 h-12 bg-red-400 rounded-full" />
-				{/each}
-			</div>
-			<div class="flex flex-col items-center">
-				{#each Array(pits) as _, i}
-					<div class="m-1.5 w-12 h-12 bg-red-400 rounded-full" />
-				{/each}
-			</div>
+		<div class="h-12 rounded-full" style:background-color={pitColor} />
+		<div class="grid grid-cols-{cols}">
+			{#each Array(cols) as _}
+				<div class="flex flex-col items-center">
+					{#each Array(pits) as _}
+						<div class="m-1.5 w-12 h-12 rounded-full" style:background-color={pitColor} />
+					{/each}
+				</div>
+			{/each}
 		</div>
-		<div class="m-auto h-12 bg-red-400 rounded-full" />
+		<div class="m-auto h-12 rounded-full" style:background-color={pitColor} />
 	</div>
 </div>
