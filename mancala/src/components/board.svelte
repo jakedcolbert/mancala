2e37aa01971399c4ee8cf12902a689f2a942c141
jakedcolbert <jake.colbert@gordon.edu>
2023-07-02 14:43:22 -0400

    Board made

---
+++ mancala/src/components/board.svelte
@@ -0,0 +1,25 @@
+<script>
+</script>
+
+<div class="box-border rounded-3xl bg-white w-32">
+	<div class="p-2">
+		<div class="m-autoe h-12 bg-red-400 rounded-full" />
+		<div class="grid grid-cols-2">
+			<div class="flex flex-col items-center">
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+			</div>
+			<div class="flex flex-col items-center">
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+				<div class="w-12 h-12 bg-red-400 rounded-full" />
+			</div>
+		</div>
+		<div class="m-auto h-12 bg-red-400 rounded-full" />
+	</div>
+</div>
---
+++ mancala/src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import Board from '../components/board.svelte';
+</script>
+
+<div class="flex flex-col" style="height:100vh">
+	<h1 class="grow-0 font-mono text-center p-10 font-black">
+		mancala for <span class="cursor-default hover:underline">babe</span>
+	</h1>
+	<div class="grow-1 self-center"><Board /></div>
+</div>
