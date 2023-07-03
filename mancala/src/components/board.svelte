f2612c17ab7de606f3ea94bcb1c774a145e3591e
jakedcolbert <jake.colbert@gordon.edu>
2023-07-02 23:58:02 -0400

    Added shadows to board

--- mancala/src/components/board.svelte
+++ mancala/src/components/board.svelte
@@ -1,23 +1,36 @@
 <script lang="ts">
 	// number of pits per side
-	export const pits: number = 5;
-	export const cols = 2;
-	const boardColor = '#FFFFFF';
-	const pitColor = '#FF5733';
+	export const pits: number = 6;
+	export let cols = 2;
+	const boardColor = '#A9E5BB';
+	const pitColor = '#FCF6B1';
 </script>
 
-<div class="box-border rounded-3xl" style:background-color={boardColor}>
+<div class="box-border custom-shadow rounded-3xl" style:background-color={boardColor}>
 	<div class="p-2">
-		<div class="h-12 rounded-full" style:background-color={pitColor} />
+		<div class="h-12 rounded-full custom-shadow-inner" style:background-color={pitColor} />
 		<div class="grid grid-cols-{cols}">
 			{#each Array(cols) as _}
-				<div class="flex flex-col items-center">
+				<div class="flex flex-col">
 					{#each Array(pits) as _}
-						<div class="m-1.5 w-12 h-12 rounded-full" style:background-color={pitColor} />
+						<div
+							class="m-2 w-12 h-12 rounded-full custom-shadow-inner"
+							style:background-color={pitColor}
+						/>
 					{/each}
 				</div>
 			{/each}
 		</div>
-		<div class="m-auto h-12 rounded-full" style:background-color={pitColor} />
+		<div class="m-auto h-12 rounded-full custom-shadow-inner" style:background-color={pitColor} />
 	</div>
 </div>
+
+<style>
+	.custom-shadow-inner {
+		box-shadow: inset 2px 4px 0px 0px rgba(0, 0, 0);
+	}
+
+	.custom-shadow {
+		box-shadow: 2px 4px 0px 0px rgba(0, 0, 0);
+	}
+</style>
--- mancala/src/routes/+page.svelte
+++ mancala/src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script>
 	import Board from '../components/board.svelte';
+	let bg = '#E3170A';
 </script>
 
-<div class="flex flex-col" style="height:100vh">
+<div class="flex flex-col" style:background-color={bg} style="height:100vh">
 	<h1 class="grow-0 font-mono text-center p-10 font-black">
 		mancala for <span class="cursor-default hover:underline">babe</span>
 	</h1>
